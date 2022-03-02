416c2bc7620637d8b3b8572e0b85a1684c98303f
hyeon98 <olreng97@gmail.com>
2022-03-02 11:18:52 +0900

    init

---
+++ radio button/radio button/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  radio button
+//
+//  Created by 정현석 on 2022/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
