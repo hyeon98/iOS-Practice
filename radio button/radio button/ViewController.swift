320b22762259d6fdc7fae1a61d574e53dc94308d
hyeon98 <olreng97@gmail.com>
2022-03-08 13:40:31 +0900

    20220308_1

--- radio button/radio button/ViewController.swift
+++ radio button/radio button/ViewController.swift
@@ -11,9 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let button = UIButton()
+        // button.setTitle("O", for: .normal)
+        button.setImage(UIImage(systemName: "bubble.left"), for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .darkGray
+        // button.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.view.addSubview(button)
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        button.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+
+        button.addTarget(self, action: #selector(onClickButton(_:)), for: .touchUpInside)
+
+
+        
+        let label = UILabel()
+        label.text = "This is My Label"
+        label.backgroundColor = .white
+        
+        self.view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.leftAnchor.constraint(equalTo: button.rightAnchor, constant: 10).isActive = true
+        label.centerYAnchor.constraint(equalTo:button.centerYAnchor).isActive = true 
     }
 
-
+    @objc internal func onClickButton(_ sender: Any) {
+        if sender is UIButton {
+            print("_hyeon button")
+        }
+    }
 }
 
